30cd7a4e8bbe0d02a23304ed186ef52f90dea28c
yogesh-karki <kulchan.yogesh2@gmail.com>
2024-04-25 22:12:17 +0545

    initial

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function openMenu() {
+  document.querySelector("header .menu").classList.add("active");
+}
+
+function closeMenu() {
+  document.querySelector("header .menu").classList.remove("active");
+}
